c6ded474634b88eb5e78665a4fe73c40deda1f85
程同学 <zenos.chen@foxmail.com>
2024-10-11 21:04:49 +0800

    下拉列表

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input class="search-input"
+           :value="searchValue"
+           @change="onChange" />
+    <div class="dropdown-list">
+      <div class="list-item"
+           v-for="item in listData"
+           :key="item">{{ item }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        searchValue: '',
+        listData: [
+          'apple',
+          'peach',
+          'banana'
+        ],
+        originListData: [
+          'apple',
+          'peach',
+          'banana'
+        ]
+      }
+    },
+    methods: {
+      onChange(e) {
+        console.log('onchange');
+        console.log(e.target.value);
+        this.searchValue = e.target.value;
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+
+        console.log('listData', this.listData);
+
+        const temp = this.originListData.filter(item => {
+          if (item.includes(this.searchValue)) {
+            return true // 留下
+          }
+          return false // 不留下
+        })
+
+        console.log('temp: ', temp);
+
+        this.listData = temp;
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    width: fit-content;
+    margin: 0 auto;
+    margin-top: 10vh;
+  }
+
+  .search-input {
+    width: 300px;
+    height: 40px;
+
+    font-size: 20px;
+  }
+
+  .dropdown-list {
+    box-shadow: 0 0 4px 0px lightgray;
+    border-radius: 4px;
+    padding: 4px;
+    width: 300px;
+
+    margin-top: 10px;
+  }
+
+  .list-item {
+    border-bottom: 1px solid gray;
+    height: 35px;
+
+    text-align: center;
+    line-height: 35px;
+  }
+
+  .list-item:last-child {
+    border-bottom: none;
+  }
 </style>
